0dae0c15838b2d74fe9d67bea81f24d801a0ea16
kr1ston <13615153@qq.com>
2025-02-07 08:55:55 +0800

    feat: 增加开屏动画(andrioid开屏有问题)

--- App.tsx
+++ App.tsx
@@ -1,9 +1,14 @@
 import 'react-native-gesture-handler';
-import React from 'react';
+import React, {useEffect} from 'react';
 import {GestureHandlerRootView} from 'react-native-gesture-handler';
 import Navigation from './src/navigation/index';
+import SplashScreen from 'react-native-splash-screen';
 
 function App(): React.JSX.Element {
+  useEffect(() => {
+    SplashScreen.hide();
+  }, []);
+
   return (
     <GestureHandlerRootView style={{flex: 1}}>
       <Navigation />
